fffc059545a40ddd117344c4c4edb2942c05b886
l3trungkjen <l3trungkjen@gmail.com>
2015-04-24 15:42:20 +0700

    Make User Courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ActiveRecord::Base
+  has_many :course_subjects, dependent: :destroy
+  has_many :user_courses, dependent: :destroy
+  has_many :subjects, through: :course_subjects
 end
--- app/models/user_course.rb
+++ app/models/user_course.rb
@@ -1,4 +1,10 @@
 class UserCourse < ActiveRecord::Base
   belongs_to :user
   belongs_to :course
+
+  validates :user_id, presence: true
+  validates :course_id, presence: true
+  validates :user_id, uniqueness: {scope: :course_id}
+
+  scope :not_finish, ->{where status: false}
 end
